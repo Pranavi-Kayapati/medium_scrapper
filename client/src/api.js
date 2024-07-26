089d6be3c43ab6677ebb5f505204c7b1e234a7ec
Jeb50 <119408915+Pranavi-Kayapati@users.noreply.github.com>
2024-07-26 19:28:09 +0530

    add

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8001";
+const API_URL = "https://medium-scrapper-server.onrender.com";
 export const scrapeArticles = async (topic) => {
   try {
     const response = await fetch(`${API_URL}/scrape`, {
--- client/src/components/ArticleScraper.jsx
+++ client/src/components/ArticleScraper.jsx
@@ -24,7 +24,7 @@ const ArticleScraper = () => {
     } catch (err) {
       console.log("err", err);
       setIsError(true);
-      setErrorMessage(err);
+      setErrorMessage(err.Error);
     }
   };
 
